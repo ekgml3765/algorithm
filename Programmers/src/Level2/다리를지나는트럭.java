3c00d3ff18b2cdc037a755b1e2192b0a726d93ec
ekgml3765 <51144317+ekgml3765@users.noreply.github.com>
2021-06-30 15:10:13 +0900

    Create 다리를지나는트럭.java

---
+++ Programmers/src/Level2/다리를지나는트럭.java
@@ -0,0 +1,73 @@
+package Level2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 다리를지나는트럭 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int bridge_length = 100;
+		int weight = 100;
+		int[] truck_weights = { 10,10,10,10,10,10,10,10,10,10 };
+		solution(bridge_length, weight, truck_weights);
+	}
+
+	static class Truck{
+		int truckWeight;
+		int arriveTime;
+		public Truck(int truckWeight, int arriveTime) {
+			this.truckWeight = truckWeight;
+			this.arriveTime = arriveTime;
+		}
+		
+	}
+	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+		int answer = 0;
+		Queue <Truck> queue = new LinkedList<Truck>();
+		for (int i = 0; i < truck_weights.length; i++) {
+			queue.add(new Truck(truck_weights[i], 0));
+		}
+		int time = 1;
+		int IngCnt = 0; //건널 수 있는 갯수
+		int IngWeight = 0; //건널 수 있는 무게
+		Queue <Truck> bridge = new LinkedList<Truck>();
+		while(true) {
+			//내릴 트럭
+			if(!bridge.isEmpty()) {
+				if(bridge.peek().arriveTime == time) {
+					IngCnt--;
+					IngWeight-= bridge.peek().truckWeight;
+					bridge.poll();
+				}	
+			}
+			
+			//트럭이 다리에 탈 수 있다면
+			if(!queue.isEmpty()) {
+				Truck truck = queue.peek();
+				if( IngWeight + truck.truckWeight <= weight && IngCnt <= bridge_length ) {
+					Truck outTruck = queue.poll();
+					outTruck.arriveTime = time + bridge_length; //내릴 시간 = 올라탄 시간 + 다리길이
+					bridge.add(outTruck);
+					IngCnt++; //갯수++
+					IngWeight += outTruck.truckWeight;		
+				}
+			}
+		
+			if(bridge.isEmpty() && queue.isEmpty())
+				break;
+			time++;
+		}
+		answer = time;
+		System.out.println(time);
+		// 먼저 이동할 택시를 큐에 담고 큐에서 택시가 다 빠질때까지
+		// 매 초마다 다리에 택시를 올릴 수 있을때까지 올려
+		// 그리고 택시들이 내려야할 시간을 봐. 그시간이 되면 다리에서 빼
+
+		return answer;
+	}
+
+}
