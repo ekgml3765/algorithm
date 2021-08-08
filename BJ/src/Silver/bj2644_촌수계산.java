141317d20f0efbfd0bcccca50d13d0d424ddbd1b
ekgml3765 <ekgml3765@naver.com>
2021-08-08 12:28:27 +0900

    0808_bj2644_촌수계산_BFS인접리스트버전

--- BJ/src/Silver/bj2644_촌수계산.java
+++ BJ/src/Silver/bj2644_촌수계산.java
@@ -3,44 +3,63 @@ package Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class bj2644_촌수계산 {
 
-	static int N, map[][], endR, endC, ans;
+	static int N, endR, endC, ans;
 	static boolean visit[];
+	static List<Integer> list[];
+	static class Node{
+		int num, cnt;
+		public Node(int num, int cnt) {
+			this.num = num;
+			this.cnt = cnt;
+		}	
+	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(in.readLine());
-		map = new int[N][N];
 		visit = new boolean[N];
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		endR = Integer.parseInt(st.nextToken())-1;
 		endC = Integer.parseInt(st.nextToken())-1;
 		int M = Integer.parseInt(in.readLine());
-		for (int i = 0; i < M ; i++) {
+		list = new ArrayList[N];
+		for (int i = 0; i < N; i++) {
+			list[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(in.readLine());
 			int a = Integer.parseInt(st.nextToken())-1;
 			int b = Integer.parseInt(st.nextToken())-1;
-			map[a][b] = 1;
-			map[b][a] = 1;
-		}//입력 끝
-		ans = Integer.MAX_VALUE;
-		visit[endR] = true;
-		dfs(endR, 0);
-		System.out.println(ans = (ans==Integer.MAX_VALUE)? -1 : ans);
+			list[a].add(b);
+			list[b].add(a);
+		}	
+		ans = -1;
+		bfs(endR);
+		System.out.println(ans);
 	}
-
-	private static void dfs(int go, int cnt) {
-		if(go == endC) {
-			ans = Math.min(cnt, ans);
-			return;
-		}
-		
-		for (int j = 0; j < N; j++) {
-			if(map[go][j] == 1 && !visit[j]) {
-				visit[j] = true;
-				dfs(j, cnt+1);
+	private static void bfs(int go) {
+		Queue<Node> queue = new LinkedList<Node>();
+		visit[go] = true;
+		queue.add(new Node(go, 0));
+		while(!queue.isEmpty()) {
+			Node node = queue.poll();
+			if(node.num == endC) {
+				ans = node.cnt;
+				return;
+			}
+			for (int i = 0; i < list[node.num].size(); i++) {
+				int num = list[node.num].get(i);
+				if(!visit[num]) {
+					visit[num] = true;
+					queue.add(new Node(num, node.cnt+1));
+				}
 			}
 		}
 	}
