93b391d05c7db14149a560baebe8afd9de0c8a68
ekgml3765 <ekgml3765@naver.com>
2021-08-13 18:52:41 +0900

    0813_bj12904_A와B

--- BJ/src/Gold/bj12904_A와B.java
+++ BJ/src/Gold/bj12904_A와B.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 public class bj12904_A와B {
 
-	static String T;
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuffer S = new StringBuffer(in.readLine());
