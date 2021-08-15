93ca1d2149c4b5ca04fe5fb85d83e970f80e17db
ekgml3765 <ekgml3765@naver.com>
2021-08-15 14:16:05 +0900

    0815_bj11399_ATM

---
+++ BJ/src/Silver/bj11399_ATM.java
@@ -0,0 +1,28 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class bj11399_ATM {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int arr[] = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int sum = 0;
+		int ans = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			ans += sum;
+		}
+		System.out.println(ans);
+	}
+}
