33683df2c8275a4e82f1502540b924fe1b261236
ekgml3765 <ekgml3765@naver.com>
2021-08-19 17:34:42 +0900

    0819_프로그래머스_숫자문자열과영단어

---
+++ Programmers/src/Level1/숫자문자열과영단어.java
@@ -0,0 +1,11 @@
+package Level1;
+
+class 숫자문자열과영단어 {
+    public int solution(String s) {
+        String arr[] = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        for(int i = 0 ; i < arr.length; i++){
+            s = s.replace(arr[i], Integer.toString(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
