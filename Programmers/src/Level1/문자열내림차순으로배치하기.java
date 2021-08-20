577a6131473f56ae917925cd5217025d4f77ec6c
ekgml3765 <ekgml3765@naver.com>
2021-08-20 11:13:07 +0900

    0820_프로그래머스_문자열내림차순으로배치하기

---
+++ Programmers/src/Level1/문자열내림차순으로배치하기.java
@@ -0,0 +1,12 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class 문자열내림차순으로배치하기 {
+	 public String solution(String s) {
+	        char arr[] = s.toCharArray();
+	        Arrays.sort(arr);
+	        StringBuilder sb = new StringBuilder(new String(arr,0,arr.length));
+	        return sb.reverse().toString();
+	    }
+}
