02c0dd9e7dd8cda907bae653961af68d698eec8f
ekgml3765 <ekgml3765@naver.com>
2021-08-20 13:07:46 +0900

    0820_프로그래머스_서울에서김서방찾기

---
+++ Programmers/src/Level1/서울에서김서방찾기.java
@@ -0,0 +1,14 @@
+package Level1;
+
+public class 서울에서김서방찾기 {
+    public String solution(String[] seoul) {
+        int idx = 0;
+        for(int i = 0 ; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                idx = i;
+                break;
+            }
+        }
+        return "김서방은 "+idx+"에 있다";
+    }
+}
