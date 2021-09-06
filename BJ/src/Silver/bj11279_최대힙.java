1443cdd92af0b86ebde6cf9f30047c27f92acc2c
ekgml3765 <ekgml3765@naver.com>
2021-09-06 14:27:11 +0900

    0906_bj11279_최대힙

---
+++ BJ/src/Silver/bj11279_최대힙.java
@@ -0,0 +1,24 @@
+package Silver;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class bj11279_최대힙 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+		for (int i = 0; i < n; i++) {
+			int num = sc.nextInt();
+			if(num == 0) {
+				if(!queue.isEmpty())
+					System.out.println(queue.poll());
+				else
+					System.out.println(0);
+				continue;
+			}
+			queue.add(num);
+		}
+	}
+}
