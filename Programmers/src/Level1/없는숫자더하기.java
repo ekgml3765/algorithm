0c01220bd4d02251a4509ec4f28f76a0979edf68
ekgml3765 <ekgml3765@naver.com>
2021-09-21 18:57:58 +0900

    0921_프로그래머스_없는숫자더하기

---
+++ Programmers/src/Level1/없는숫자더하기.java
@@ -0,0 +1,18 @@
+package Level1;
+
+public class 없는숫자더하기 {
+
+	public int solution(int[] numbers) {
+		int answer = 0;
+		boolean num[] = new boolean[10];
+		for (int i = 0; i < numbers.length; i++) {
+			num[numbers[i]] = true;
+		}
+		for (int i = 0; i < num.length; i++) {
+			if (!num[i])
+				answer += i;
+		}
+		return answer;
+	}
+
+}
