442bc31157a84b0ee446e50908c860480afc641d
ekgml3765 <ekgml3765@naver.com>
2021-09-24 19:21:31 +0900

    0924_프로그래머스_위클리챌린지_7주차

--- Programmers/src/Level2/위클리챌린지_7주차.java
+++ Programmers/src/Level2/위클리챌린지_7주차.java
@@ -1,18 +1,7 @@
 package Level2;
-
 import java.util.HashMap;
-
-public class 위클리챌린지_7주차 {
-
-	public static void main(String[] args) {
-		int[] enter = { 1, 4, 2, 3 };
-		int[] leave = { 2, 1, 3, 4 };
-		// int[] enter = {1,3,2};
-		// int[] leave = {1,2,3};
-		solution(enter, leave);
-	}
-
-	public static int[] solution(int[] enter, int[] leave) {
+class 위클리챌린지_7주차 {
+    public int[] solution(int[] enter, int[] leave) {
 		int[] answer = new int[enter.length];
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 		HashMap<Integer, Integer> index = new HashMap<Integer, Integer>();
@@ -39,5 +28,5 @@ public class 위클리챌린지_7주차 {
 			}
 		}
 		return answer;
-	}
+    }
 }
