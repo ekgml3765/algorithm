89ba2e198f47224b76fec3d6c0c68c8e3da00f06
ekgml3765 <ekgml3765@naver.com>
2021-10-14 21:23:51 +0900

    1014_bj15655_N과M_6

---
+++ BJ/src/Silver/bj15655_N과M_6.java
@@ -0,0 +1,41 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class bj15655_N과M_6 {
+
+	static int N, M, arr[];
+	static int[] sel;
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(in.readLine());
+		arr = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		sel = new int[M];
+		comb(0, 0);
+	}
+
+	private static void comb(int idx, int num) {
+		if (idx == M) {
+			for (int i = 0; i < sel.length; i++) {
+				System.out.print(sel[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = num; i < N; i++) {
+			sel[idx] = arr[i];
+			comb(idx + 1, i + 1);
+		}
+	}
+}
