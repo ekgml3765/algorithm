bcb46ea23f5a4bbf4793c52649922aee89d52e1c
ekgml3765 <ekgml3765@naver.com>
2021-10-24 23:07:19 +0900

    1024_나머지가1이되는수찾기

---
+++ Programmers/src/Level1/나머지가1이되는수찾기.java
@@ -0,0 +1,15 @@
+package Level1;
+
+public class 나머지가1이되는수찾기 {
+
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i < 1000000; i++){
+            if(n%i == 1){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
