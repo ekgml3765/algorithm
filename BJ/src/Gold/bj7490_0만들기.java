144c126dc9b251ecc1629499ba132f8c5f15c3ec
ekgml3765 <ekgml3765@naver.com>
2022-03-23 00:14:40 +0900

    0322_bj7490_0만들기_수정

--- BJ/src/Gold/bj7490_0만들기.java
+++ BJ/src/Gold/bj7490_0만들기.java
@@ -3,62 +3,39 @@ package Gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class bj7490_0만들기 {
 
 	static int N;
 	static List<String> ans;
-	static String op[] = {"+", "-", " "};
+	static StringBuilder sb;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(in.readLine());
 		for(int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(in.readLine());
-			ans = new ArrayList<>();
-			dfs(1, "1");
-			Collections.sort(ans);
-			for(String s : ans) {
-				System.out.println(s);
-			}
-			System.out.println();
-			
+			sb = new StringBuilder();
+			dfs(1, 1, 0, 1, "1");
+			System.out.println(sb);
 		}
 	}
 	
 	
-	private static void dfs(int num, String s) {
-		if(num == N) {
-			String express = s.replaceAll(" ", "");
-			if(cal(express))
-				ans.add(s);
+	private static void dfs(int idx, int num, int sum, int op, String express) {
+		if(idx == N) {
+			sum += (num*op);
+			if(sum == 0)
+				sb.append(express + "\n");
 			return;
 		}
 		
-		for(int i = 0; i < 3; i++) {
-			dfs(num+1, s+op[i]+Integer.toString(num+1));
-		}
-	}
-
-
-	private static boolean cal(String express) {
-		StringTokenizer st = new StringTokenizer(express, "-|+", true);
-		int sum = Integer.parseInt(st.nextToken());
-		while(st.hasMoreElements()) {
-			String s = st.nextToken();
-			if(s.equals("+")) {
-				sum += Integer.parseInt(st.nextToken());
-			}else {
-				sum -= Integer.parseInt(st.nextToken());
-			}
-		}
+		dfs(idx+1, num*10+(idx+1), sum, op, express+" "+Integer.toString(idx+1));
+		dfs(idx+1, idx+1, sum+(num*op), 1, express+"+"+Integer.toString(idx+1));
+		dfs(idx+1, idx+1, sum+(num*op), -1, express+"-"+Integer.toString(idx+1));
 		
-		if(sum == 0)
-			return true;
-		return false;
+		
 	}
+
 }
